be0a7a30bc223c334d35797cb0d8160b85f577ab
Alexander Tiedmann <alexander.tiedmann@googlemail.com>
2016-10-23 18:36:15 +0200

    Abschluss Übungsblatt 2

--- Matrix/src/Matrix.java
+++ Matrix/src/Matrix.java
@@ -1,44 +1,170 @@
 import java.util.Arrays;
 public class Matrix {
-
-    private float[][] matrix = new float[][];
-
-    public boolean equals(float[][] matrix1){
-        
-        if ( Arrays.equals(matrix1,matrix) ){
+    private int columns,rows;
+    private float[][] matrix;
+    /**
+     * Konstruktor fuer die Matrix
+     * @param ma ein zwei-dimensionales float-Array
+     */
+    public Matrix (float[][] ma){
+        matrix = ma;
+        setColumns(ma);
+        setRows(ma);
+    }
+    /**
+     *Ginbt zurueck ob zwei Matrizen gleich sind
+     * @param matrix1 Eingabematrix
+     * @return Gleich oder nicht gleich
+     */
+    public boolean equals(Matrix matrix1){
+        if ( Arrays.deepEquals(matrix1.getMatrix(),matrix) ){
             return true;
         }else {
             return false;
         }
     }
-
-    public float[][] add(float[][] matrix1){
-
-        return matrix1;
+    /**
+     * Addiert die Eingabematrix mit der Matrix und liefert das Ergebnis
+     * @param matrix1 Eingabematrix
+     * @return Ergebnismatrix
+     */
+    public float[][] add(Matrix matrix1){
+        //wenn die Matrizen unterschiedliche Dimensionen haben wird eine Fehlermatrix zurueckgegeben
+        if (isSameDimension(matrix1)){
+            float[][] ergmatrix = new float[rows][columns];
+            for (int i = 0; i < rows;i++){
+                for (int j = 0; j < columns; j++){
+                    ergmatrix[i][j] = matrix[i][j] + matrix1.getMatrix()[i][j];
+                }
+            }
+            return ergmatrix;
+        }else{
+            //zurueckgeben der Fehlermatrix
+            return getFehlermatrix();
+        }
     }
-
-    public float[][] multiplayScalar(float[][] matrix, float skalar){
-
+    /**
+     * Multipliziert die Matrix mit einem Skalar und gibt die Ergebnismatrix zurueck
+     * @param skalar Skalar zum mutliplizieren
+     * @return Ergebnisnmatrix
+     */
+    public float[][] multiplyScalar(float skalar){
+        //Wenn die Zeilen null sind (wenn die Spalten in den Zeilen unterschiedlich sind) dann Fehlermatrix zurueckgeben
+        if (columns != 0) {
+            float[][] ergmatrix = new float[rows][columns];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    ergmatrix[i][j] = matrix[i][j] * skalar;
+                }
+            }
+            return ergmatrix;
+        }else{
+            //zurueckgegben der Fehlermatrix
+            return getFehlermatrix();
+        }
+    }
+    /**
+     * Multipliziert zwei Matrizen und gibt die Ergebnismatrix zurueck
+     * @param matrix1 Eingabematrix
+     * @return Ergebnismatrix
+     */
+    public float[][] multiply(Matrix matrix1){
+        //Wenn die Spalten der Matrix ungleich der Zeilen der Eingabematrix sind wird eine Fehlermatrix zurueck gegeben
+        if ( columns == matrix1.getRows()){
+            float[][] ergebnis = new float[rows][matrix1.getColumns()];
+            for (int i = 0; i < matrix1.getRows();i++){
+                for (int j = 0; j < matrix1.getColumns();j++){
+                    ergebnis[i][j] = 0;
+                    for (int k = 0; k < columns;k++){
+                        ergebnis[i][j] += matrix[i][k] * matrix1.getMatrix()[k][j];
+                    }
+                }
+            }
+            return ergebnis;
+        }else{
+            return getFehlermatrix();
+        }
+    }
+    /**
+     * gibt die Anzahl an Spalten zurueck
+     * @return Spaltenanzahl
+     */
+    public int getColumns(){
+        return columns;
+    }
+    /**
+     * Gibt die Anzahl der Zeilen zurueck
+     */
+    public int getRows(){
+        return rows;
+    }
+    /**
+     * Setzt die Anzahl der Zeilen
+     * @param ma Matrix fuer die die Zeilenanzahl bestimmt wird
+     */
+    private void setRows(float[][] ma){
+        rows = ma.length;
+    }
+    /**
+     * Setzt die Anzahl der Spalten
+     * bei Fehler wird eine 0 gesetzt
+     * @param ma Matrix fuer die die Spaltenanzahl gesetzt wird
+     */
+    private void setColumns(float[][] ma){
+        if (checkSameRowLength(ma)) {
+            columns = ma[0].length;
+        }else{
+            columns = 0;
+        }
+    }
+    /**
+     * Gibt die Matrix zurueck
+     * @return Matrix
+     */
+    public float[][] getMatrix(){
         return matrix;
     }
 
-    public float[][] multiplay(float[][] matrix1){
-
-        return matrix1;
+    /**
+     * erzeugt eine Fehlermatrix und gibt diese zurueck
+     * @return Fehlermatrix
+     */
+    private float[][] getFehlermatrix(){
+        float[][] erg = new float[1][2];
+        erg[0][0] = 0;
+        erg[0][1] = 0;
+        return erg;
     }
-
-    private int getColumns(){
-
-        return 1;
+    /**
+     * Gibt an ob die Matrix die selbe Dimension hat wie die Eingabematrix
+     * @param matrix1 Eingabematrix
+     * @return Ist gleich oder ist nicht gleich
+     */
+    private boolean isSameDimension(Matrix matrix1){
+        if ( rows == matrix1.getRows()){
+            if ( columns == matrix1.getColumns()){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
-
-    private int getRows(){
-
-        return 1;
-    }
-
-    private boolean isSameDimension(float[][] matrix1){
-
-        return true;
+    /**
+     * prueft ob eine Matrix gleich lange Zeilen hat
+     * @param ma zu pruefende Matrix
+     * @return gleich lange Zeilen oder nicht
+     */
+    private boolean checkSameRowLength(float[][] ma){
+        boolean wahr = true;
+        for (int i = 0; i < ma.length; i++){
+            for (int j = 0; j < ma[i].length;j++){
+                if (ma[i].length != ma[j].length){
+                    wahr = false;
+                }
+            }
+        }
+        return wahr;
     }
 }
--- Matrix/src/MatrixTest.java
+++ Matrix/src/MatrixTest.java
@@ -1,61 +1,167 @@
+import com.sun.corba.se.impl.orbutil.CorbaResourceUtil;
+
 import static org.junit.Assert.*;
-
 public class MatrixTest {
-    private float[][] matrix1 = new float[2][2];
-    private float[][] matrix2 = new float[2][1];
-    private float[][] matrix3 = new float[2][2];
-
+    private Matrix ma1;
+    private Matrix ma2;
+    private Matrix ma3;
+    private Matrix ma4;
+    private Matrix ma5;
+    /**
+     * setzten der Werte fuer die Matrizen
+     * Matrix1 und 3 sind gleich
+     * Matrix1,3 und 4 haben die gleiche Dimension
+     * Matrix2 hat eine andere Dimension als alle anderen
+     * Matrix5 hat ungleich lange Zeilen und eine Zeile mehr als die anderen
+     * @throws Exception
+     */
     @org.junit.Before
     public void setUp() throws Exception {
-        Matrix ma = new Matrix();
+        float[][] matrix1 = new float[2][2];
         matrix1[0][0] = 1;
         matrix1[0][1] = 2;
         matrix1[1][0] = 3;
         matrix1[1][1] = 4;
+        ma1 = new Matrix(matrix1);
 
+        float[][] matrix2 = new float[2][1];
         matrix2[0][0] = 5;
         matrix2[1][0] = 6;
+        ma2 = new Matrix(matrix2);
 
+
+        float[][] matrix3 = new float[2][2];
         matrix3[0][0] = 1;
         matrix3[0][1] = 2;
         matrix3[1][0] = 3;
         matrix3[1][1] = 4;
+        ma3 = new Matrix(matrix3);
+
+
+        float[][] matrix4 = new float[2][2];
+        matrix4[0][0] = 1;
+        matrix4[0][1] = 1;
+        matrix4[1][0] = 1;
+        matrix4[1][1] = 1;
+        ma4 = new Matrix(matrix4);
+
+
+        float[][] matrix5 = new float[3][];
+        matrix5[0] = new float[2];
+        matrix5[1] = new float[1];
+        matrix5[2] = new float[2];
+        matrix5[0][0] = 1;
+        matrix5[0][1] = 2;
+        matrix5[1][0] = 3;
+        matrix5[2][0] = 2;
+        matrix5[2][1] = 3;
+        ma5 = new Matrix(matrix5);
     }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-
-    }
-
-    @org.junit.Test
     /**
-     * Test des Fehlerfalls (ungleiche Dimensionen
+     * Test des Fehlerfalls (Matrizen sind nicht gleich)
      */
-    public void equalsUngleichDimension() throws Exception {
-        assertFalse("Fehlgeschlagen", Matrix.equals(matrix1,matrix2));
-    }
-
     @org.junit.Test
+    public void equalsUngleich() throws Exception {
+        assertFalse("Die Matrizen sind nicht gleich", ma1.equals(ma4));
+    }
     /**
-     * Test des Normalfalls
+     * Test des Normalfalls (Matrizen sind gleich)
      */
+    @org.junit.Test
     public void equalsNormalfall() throws Exception {
-        assertTrue("Erfolgreich", Matrix.equals(matrix1,matrix3));
+        assertTrue("Die Matrizen sind gleich", ma1.equals(ma3));
     }
-
+    /**
+     * Test des Fehlerfalls (Dimensionen sind ungleich)
+     */
     @org.junit.Test
-    public void add() throws Exception {
-
+    public void equalsUngleichDimension() throws Exception {
+        assertFalse("Die Matrizen haben unterschiedliche Dimensionen", ma1.equals(ma2));
     }
-
+    /**
+     * Test des Normalfalls (Ergebnis stimmt mit dem Erwarteten ueberein)
+     * @throws Exception
+     */
     @org.junit.Test
-    public void multiplayScalar() throws Exception {
-
+    public void addNormalfall() throws Exception {
+        float[][] expectedMatrix = new float[2][2];
+        expectedMatrix[0][0] = 2;
+        expectedMatrix[0][1] = 3;
+        expectedMatrix[1][0] = 4;
+        expectedMatrix[1][1] = 5;
+        assertArrayEquals(expectedMatrix,ma1.add(ma4));
     }
-
+    /**
+     * Test des Fehlerfalls (Spalten sind ungleich)
+     * @throws Exception
+     */
     @org.junit.Test
-    public void multiplay() throws Exception {
-
+    public void addSpaltenUngleich() throws Exception {
+        float[][] expectedMatrix = new float[1][2];
+        expectedMatrix[0][0] = 0;
+        expectedMatrix[0][1] = 0;
+        assertArrayEquals(expectedMatrix,ma1.add(ma2));
     }
-
+    /**
+     * Test des Fehlerfalls (Zeilen sind ungleich)
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void addZeilenUngleich() throws Exception {
+        float[][] expectedMatrix = new float[1][2];
+        expectedMatrix[0][0] = 0;
+        expectedMatrix[0][1] = 0;
+        assertArrayEquals(expectedMatrix,ma1.add(ma5));
+    }
+    /**
+     * Test des Normalfalls (Ergebnis und Erwartung stimmen ueberein)
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void multiplyScalarNormalfall() throws Exception {
+        float skalar = 1;
+        float[][] expectedMatrix = new float[2][2];
+        expectedMatrix[0][0] = 1;
+        expectedMatrix[0][1] = 2;
+        expectedMatrix[1][0] = 3;
+        expectedMatrix[1][1] = 4;
+        assertArrayEquals(expectedMatrix,ma1.multiplyScalar(skalar));
+    }
+    /**
+     * Test des Fehlerfalls (Spalten=0, da ungleiche Spaltenzahlen in den Zeilen)
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void multiplyScalarZeilenGleichNull() throws Exception {
+        float skalar = 1;
+        float[][] expectedMatrix = new float[1][2];
+        expectedMatrix[0][0] = 0;
+        expectedMatrix[0][1] = 0;
+        assertArrayEquals(expectedMatrix,ma5.multiplyScalar(skalar));
+    }
+    /**
+     * Test des Normalfalls (Zeilen von Matrix1 gleich Spaltenanzahl Matrix2)
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void multiplyNormalfall() throws Exception {
+        float[][] expectedMatrix = new float[2][2];
+        expectedMatrix[0][0] = 3;
+        expectedMatrix[0][1] = 3;
+        expectedMatrix[1][0] = 7;
+        expectedMatrix[1][1] = 7;
+        assertArrayEquals(expectedMatrix,ma1.multiply(ma4));
+    }
+    /**
+     * Test des Fehlerfalls (Zeilen von Matrix1 ungleich Spaltenanzahl Matrix2)
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void multiplyFehlerSpaltenZeilen() throws Exception {
+        float[][] expectedMatrix = new float[1][2];
+        expectedMatrix[0][0] = 0;
+        expectedMatrix[0][1] = 0;
+        assertArrayEquals(expectedMatrix,ma1.multiply(ma5));
+    }
+ 
 }
