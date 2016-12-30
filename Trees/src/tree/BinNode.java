41afaaf04f1696732df1dbaa6d09d60a9940e508
Alexander Tiedmann <alexander.tiedmann@googlemail.com>
2016-12-30 15:22:13 +0100

    Anlegen Aufgabe 11

---
+++ Trees/src/tree/BinNode.java
@@ -0,0 +1,14 @@
+package tree;
+public class BinNode {
+    int data;
+    BinNode left, right;
+    // Konstruktoren
+    BinNode(int d) {
+        data = d;
+        left = right = null;
+    }
+    //zusätzlich, um Knoten direkt zu erzeugen:
+    BinNode(int d, BinNode l, BinNode r) {
+        data = d; left = l; right = r;
+    }
+}
---
+++ Trees/src/tree/BinTree.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class BinTree
+{
+    private BinNode root = null;
+    BinTree() {
+        root = null;
+    }
+    BinTree
+            (BinNode rn) {
+        root = rn
+        ;
+    }
+}
